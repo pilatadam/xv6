dd2439a719e8308bd67eed6053be7155ce79964c
Adam Pilat <xpilat2@fi.muni.cz>
2025-03-18 23:31:53 +0100

    [1] xargs: Initialize, prototype

---
+++ user/xargs.c
@@ -0,0 +1,17 @@
+#include "user.h"
+#include "kernel/param.h"
+
+int
+main(int argc, char *argv[])
+{
+
+  char *argv_arr[MAXARG];
+
+
+  // read from stdin until '\n'
+  // run command with fork + exec
+  // wait in the parent for the end of execution
+  // repeat
+
+  exit(0);
+}
