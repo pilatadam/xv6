35c5a16cd5b1a96a08d03204dbaadc1fdd7aa5ac
Adam Pilat <xpilat2@fi.muni.cz>
2025-03-19 03:12:55 +0100

    [lab1] xargs: Implementation, TO-DO: memory, refactor, finish

--- user/xargs.c
+++ user/xargs.c
@@ -1,17 +1,46 @@
-#include "user.h"
+#include "kernel/types.h"
+#include "user/user.h"
 #include "kernel/param.h"
+
 
 int
 main(int argc, char *argv[])
 {
+  char **argarr = malloc(MAXARG * sizeof(char *));
 
-  char *argv_arr[MAXARG];
+  int i = 0, j = 0, in = 0;
+  
+  // Fill arguments
+  for(int k = 1; k < argc; k++)
+    argarr[i++] = argv[k];
 
+  argarr[i] = malloc(100);
 
-  // read from stdin until '\n'
-  // run command with fork + exec
-  // wait in the parent for the end of execution
-  // repeat
+  char c;
+  while (read(0, &c, 1) > 0) {
+
+    if (c == '\n')
+      break;
+
+    if (c == ' ') {
+      if (in) {
+        argarr[i++][j] = '\0';
+        argarr[i] = malloc(100);
+        j = 0;
+        in = 0;
+      }
+    }
+    else {
+      argarr[i][j++] = c;
+      in = 1;
+    }
+  }
+
+  if (in)
+    argarr[i++][j] = '\0';
+  argarr[i] = 0;
+
+  exec(argv[1], argarr);
 
   exit(0);
 }
