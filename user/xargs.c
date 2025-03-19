3ae33c9fbff95ce39d2becb9f7ce8046298d8503
Adam Pilat <xpilat2@fi.muni.cz>
2025-03-19 19:32:05 +0100

    [lab1] xargs: Finished

--- user/xargs.c
+++ user/xargs.c
@@ -2,45 +2,118 @@
 #include "user/user.h"
 #include "kernel/param.h"
 
+void
+freeargarr(char **argarr, int arrlen)
+{
+  for(int i = 0; i < arrlen; i++)
+    free(argarr[i]);
+}
 
 int
 main(int argc, char *argv[])
 {
   char **argarr = malloc(MAXARG * sizeof(char *));
-
-  int i = 0, j = 0, in = 0;
+  if (argarr == 0) {
+    fprintf(2, "Error: malloc failed!\n");
+    exit(1);
+  }
+  int arrlen = 0;
   
-  // Fill arguments
+  /** Fill the array with arguments to the command to be run.
+   *  Arguments are same for each input line.
+   */
   for(int k = 1; k < argc; k++)
-    argarr[i++] = argv[k];
+    argarr[arrlen++] = argv[k];
 
-  argarr[i] = malloc(100);
+  argarr[arrlen] = malloc(100);
+  if (argarr[arrlen] == 0) {
+    fprintf(2, "Error: malloc failed!\n");
+    freeargarr(argarr, arrlen);
+    exit(1);
+  }
 
+  /**
+   * [argind]: index of the argument being parsed
+   * [strind]: index of the string in the argument
+   * [in]:     1 if we are reading word chars, 0 for spaces...
+   */
+  int argind, strind, in;
   char c;
-  while (read(0, &c, 1) > 0) {
+  int readbytes = 1;
+  while (readbytes) {
+    argind = arrlen;
+    strind = 0;
+    in = 0;
 
-    if (c == '\n')
+    /**
+     *  Parse the line and divide it into multiple arguments
+     */
+    while ((readbytes = read(0, &c, 1)) > 0) {
+
+      if (c == '\n')
+        break;
+
+      if (c == ' ') {
+        /**
+         *  If we were reading argument and read ' ',
+         *  so we are likely at the end of argument
+         */
+        if (in) {
+          argarr[argind++][strind] = '\0';
+          /**
+           *  Allocate memory for next argument if not
+           *  already allocated, handle malloc failure
+           */
+          if (argind >= arrlen) {
+            argarr[argind] = malloc(100);
+            if (argarr[argind] == 0) {
+              fprintf(2, "Error: malloc failed!\n");
+              freeargarr(argarr, arrlen);
+              exit(1);
+            }
+          }
+          strind = 0;
+          in = 0;
+        }
+      }
+      else {
+        argarr[argind][strind++] = c;
+        in = 1;
+      }
+    
+    }
+
+    if (in)
+      argarr[argind++][strind] = '\0';
+    /**
+     *  Set the last argument in the array to NULL, since
+     *  argv[argc] == NULL
+     */
+    argarr[argind] = 0;
+
+    /**
+     *  Break when no new arguments read
+     */
+    if (arrlen == argind)
       break;
 
-    if (c == ' ') {
-      if (in) {
-        argarr[i++][j] = '\0';
-        argarr[i] = malloc(100);
-        j = 0;
-        in = 0;
-      }
+    if (fork() == 0) {
+      /**
+       *  Run the command in the child, handle exec failure
+       */
+      exec(argv[1], argarr);
+      fprintf(2, "Error: exec failed!\n");
+      freeargarr(argarr, arrlen);
+      exit(1);
     }
     else {
-      argarr[i][j++] = c;
-      in = 1;
+      /**
+       *  Wait in the child for the execution of the command
+       */
+      wait(0);
     }
   }
 
-  if (in)
-    argarr[i++][j] = '\0';
-  argarr[i] = 0;
-
-  exec(argv[1], argarr);
-
+  freeargarr(argarr, arrlen);
   exit(0);
 }
